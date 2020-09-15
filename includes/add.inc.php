03b41ffc40dcc1a8e7f66219f477a5cd51bff845
Luca Kwon <hyeoklucakwon@Hyeoks-MacBook-Pro.local>
2020-09-15 14:06:10 +0200

    VC

--- includes/add.inc.php
+++ includes/add.inc.php
@@ -1,5 +1,13 @@
 <?php
 
+session_start();
+if (isset($_SESSION['id'])) {}
+else {
+  header("Location: index.php?error=nosession");
+  exit();
+}
+
+
 if (isset($_POST['add'])) {
   require 'dbh.inc.php';
 
@@ -11,14 +19,33 @@ if (isset($_POST['add'])) {
     exit();
   }
   else {
-    $sqlinsert = "INSERT INTO ";
+    $sqlselect = "SELECT sportsID FROM sports WHERE sportsName = ?;";
     $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
+    if (!mysqli_stmt_prepare($stmt, $sqlselect)) {
       header("Location: ../AddInfo.php?error=sqlerror");
       exit();
     }
     else {
-      $
+      mysqli_stmt_bind_param($stmt, "s", $sport);
+      mysqli_stmt_execute($stmt);
+
+      $result = mysqli_stmt_get_result($stmt);
+
+      if ($row = mysqli_fetch_assoc($result)) {
+        $sqlinsert = "INSERT INTO exevents (id, sportid, edate) VALUES (?, ?, ?);";
+        $rstmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($rstmt, $sqlinsert)) {
+          die("Connection failed: ".mysqli_connect_error());
+          header("Location: ../AddInfo.php?error=sql");
+          exit();
+        }
+        else {
+          mysqli_stmt_bind_param($rstmt, "iis", $_SESSION['id'], $row['sportsID'], $date);
+          mysqli_stmt_execute($rstmt);
+          header("Location: ../home.php?success=added");
+          exit();
+        }
+      }
     }
   }
 
